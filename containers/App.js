ae228a1864a405c3ae68676b38da3d630bed5c00
John Otu <johnne01@gmail.com>
2017-12-27 01:06:37 +0000

    Initial commit

---
+++ containers/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Hello from '../components/Hello';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      <Hello />
+        <p>Welcome to dlyvr</p>
+      </div>  
+    )
+  }
+}
